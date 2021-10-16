5122764f68ba9b28d0ff0ecf2a92077b184fb59b
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-16 21:31:38 +0530

    Added nav styling and Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="container">Navbar</div>;
+  return (
+    <div className="container">
+      <ul id="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/create-post">Create-post</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
